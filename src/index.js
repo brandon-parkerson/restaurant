6bd27c1a9e2902ae4ae549c1558c7bbd2821bed8
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-07-19 11:27:51 +0300

    Load tabs and image from JavaScript only.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-import pancakes from './pancakes.jpeg';
+import './style.css';
+import table from './diner-background.jpg';
+
+const header = document.querySelector(".header");
+const navigation = document.querySelector(".nav");
+const content = document.getElementById("content");
+const imgElement = document.createElement("img");
+const btnHome = document.createElement("button");
+const btnMenu = document.createElement("button");
+const btnAbout = document.createElement("button");
 
 document.addEventListener('DOMContentLoaded', () => {
-    const imgElement = document.getElementById("pancakes");
-    imgElement.src = pancakes;
-})
+    
+    loadButtons();
+    loadTable();
+
+});
+
+function loadButtons() {
+    btnHome.innerText = "Home";
+    btnMenu.innerText = "Menu";
+    btnAbout.innerText = "About";
+
+    navigation.appendChild(btnHome);
+    navigation.appendChild(btnMenu);
+    navigation.appendChild(btnAbout);
+
+    header.appendChild(navigation);
+};
+
+function loadTable() {
+    imgElement.src = table;
+    imgElement.classList.add("table");
+    content.appendChild(imgElement);
+};
+
+// function loadQuote() {
+//     const quote = document.createElement(p)
+// }
+
